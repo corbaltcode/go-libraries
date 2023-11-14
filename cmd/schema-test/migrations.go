dad0e3d241299ada00d1c0c5cf3fa869dd9fed38
Chris Hundt <chris@charlesbearllc.com>
2023-11-14 14:00:36 -0500

    Add migration check

---
+++ cmd/schema-test/migrations.go
@@ -0,0 +1,71 @@
+package main
+
+import "github.com/corbaltcode/go-libraries/migrations"
+
+var allMigrations = []migrations.NamedMigration{
+	{
+		Name: "Create a table",
+		Migration: migrations.StaticMigration([]string{
+			`CREATE TABLE table1 (
+				id serial PRIMARY KEY,
+				f1 TEXT NULL
+			)`,
+		}),
+		Reverse: migrations.StaticMigration([]string{
+			`DROP TABLE table1`,
+		}),
+	},
+	{
+		Name: "Make field not-nullable",
+		Migration: migrations.StaticMigration([]string{
+			`ALTER TABLE table1 ALTER COLUMN f1 SET NOT NULL`,
+		}),
+		Reverse: migrations.StaticMigration([]string{
+			`ALTER TABLE table1 ALTER COLUMN f1 DROP NOT NULL`,
+		}),
+	},
+	{
+		Name: "Create a dependent table",
+		Migration: migrations.StaticMigration([]string{
+			`CREATE TABLE table2 (
+				id serial PRIMARY KEY,
+				table1_id integer REFERENCES table1(id)
+			)`,
+		}),
+		Reverse: migrations.StaticMigration([]string{
+			`DROP TABLE table2`,
+		}),
+	},
+	{
+		Name: "Create a table with an enum type",
+		Migration: migrations.StaticMigration([]string{
+			`CREATE TYPE type1 AS ENUM (
+				'type1va11',
+				'type1val2'
+			)`,
+			`CREATE TABLE table3 (
+				id serial PRIMARY KEY,
+				v type1 NOT NULL
+			)`,
+		}),
+		Reverse: migrations.StaticMigration([]string{
+			`DROP TABLE table3`,
+			`DROP TYPE type1`,
+		}),
+	},
+	{
+		Name: "Add a new value to the enum type",
+		Migration: migrations.StaticMigration([]string{
+			`ALTER TYPE type1 ADD VALUE 'type1val3'`,
+		}),
+		Reverse: migrations.StaticMigration([]string{
+			`ALTER TYPE type1 RENAME TO type1_old`,
+			`CREATE TYPE type1 AS ENUM (
+			'type1va11',
+			'type1val2'
+		)`,
+			`ALTER TABLE table3 ALTER COLUMN v TYPE type1 USING v::text::type1`,
+			`DROP TYPE type1_old`,
+		}),
+	},
+}
---
+++ migrations/verify.go
@@ -0,0 +1,121 @@
+package migrations
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/jmoiron/sqlx"
+)
+
+type PostgresConfig struct {
+	Host, Port, Database, User, Password string
+}
+
+func dump(c *PostgresConfig) ([]byte, error) {
+	pgpass := fmt.Sprintf("%s:%s:%s:%s:%s", c.Host, c.Port, c.Database, c.User, c.Password)
+	tempDir, err := os.MkdirTemp(os.TempDir(), "pgdump")
+	if err != nil {
+		return nil, fmt.Errorf("Error creating temp dir: %w", err)
+	}
+	defer os.RemoveAll(tempDir)
+	passFilePath := path.Join(tempDir, ".pgpass")
+	err = os.WriteFile(passFilePath, []byte(pgpass), 0600)
+	if err != nil {
+		return nil, fmt.Errorf("Error writing pgpass: %w", err)
+	}
+	cmd := exec.Command("pg_dump",
+		"-s", // schema only
+		"-h", c.Host,
+		"-p", c.Port,
+		"-U", c.User,
+		c.Database)
+	cmd.Dir = tempDir
+	cmd.Env = append(cmd.Env, "PGPASSFILE="+passFilePath)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("Error calling pg_dump: %s. Output:\n%s", err, out)
+	}
+	return out, err
+}
+
+// Schema test expects a new empty postgres database.
+// It is destructive, so you should not call it on a database with data
+// or one that has had migrations already applied.
+// It will, for each migration:
+// 1. Apply the migration
+// 2. Reverse the migration
+// 3. Apply the migration again
+// Before and after each step it will use pg_dump to dump the database schema.
+// It will verify that:
+// A. The schema is the same after step 2 as before step 1.
+// B. The schema is the same after step 3 as after step 1.
+//
+// You must have `pg_dump` in your `PATH` to run this.
+func SchemaTest(emptyDBConfig *PostgresConfig, allMigrations []NamedMigration) error {
+	for _, v := range []string{
+		emptyDBConfig.Host,
+		emptyDBConfig.Port,
+		emptyDBConfig.Database,
+		emptyDBConfig.User,
+		emptyDBConfig.Password,
+	} {
+		if v == "" {
+			return errors.New("All PostgresConfig fields must be specified")
+		}
+	}
+	postgresConnectionString := fmt.Sprintf(
+		"host=%s port=%s password=%s sslmode=disable user=%s dbname=%s",
+		emptyDBConfig.Host, emptyDBConfig.Port, emptyDBConfig.Password, emptyDBConfig.User, emptyDBConfig.Database)
+	db, err := sqlx.Connect("postgres", postgresConnectionString)
+	if err != nil {
+		return fmt.Errorf("Error connecting: %s", err)
+	}
+	err = Migrate(db, []NamedMigration{})
+	if err != nil {
+		return fmt.Errorf("Setting up migrations table failed: %s", err)
+	}
+	for idx, migration := range allMigrations {
+		beforeMigrate, err := dump(emptyDBConfig)
+		if err != nil {
+			return fmt.Errorf("Error calling pg_dump: %s", err)
+		}
+		os.WriteFile("/tmp/beforeMigrate", beforeMigrate, 0644)
+		err = Migrate(db, allMigrations[:idx+1])
+		if err != nil {
+			return fmt.Errorf("Migration %q failed: %s", migration.Name, err)
+		}
+		afterMigrate, err := dump(emptyDBConfig)
+		if err != nil {
+			return fmt.Errorf("Error calling pg_dump: %s", err)
+		}
+		err = Rollback(db, allMigrations, idx)
+		if err != nil {
+			return fmt.Errorf("Rollback to %q failed: %s", migration.Name, err)
+		}
+		afterRollback, err := dump(emptyDBConfig)
+		if err != nil {
+			return fmt.Errorf("Error calling pg_dump: %s", err)
+		}
+		if string(beforeMigrate) != string(afterRollback) {
+			fmt.Printf("%s\n", cmp.Diff(string(beforeMigrate), string(afterRollback)))
+			return fmt.Errorf("Dump after rollback of %q did not match the dump before the migration", migration.Name)
+		}
+		err = Migrate(db, allMigrations[:idx+1])
+		if err != nil {
+			return fmt.Errorf("Migration %q failed: %s", migration.Name, err)
+		}
+		afterMigrateAgain, err := dump(emptyDBConfig)
+		if err != nil {
+			return fmt.Errorf("Error calling pg_dump: %s", err)
+		}
+		if string(afterMigrate) != string(afterMigrateAgain) {
+			fmt.Printf("%s\n", cmp.Diff(string(afterMigrate), string(afterMigrateAgain)))
+			return fmt.Errorf("Dump after re-migration of %q did not match dump after first migration", migration.Name)
+		}
+	}
+	return nil
+}
